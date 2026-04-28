5e495832d5e46039cecd93f7175a5192f3a49a0d
Leito1804 <165241969+Leito1804@users.noreply.github.com>
2026-04-28 14:42:57 -0500

    Update urls.py

---
+++ odontovros/urls.py
@@ -0,0 +1,8 @@
+"""URLs del proyecto."""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("citas.urls")),
+]
