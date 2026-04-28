e4c07275305328706495cf06e72dd0fc9b9c394c
Mauricio Paolo Lazo Franco <mauricio.lazo@estudiante.ucsm.edu.pe>
2026-04-28 14:40:34 -0500

    Clase CitaAdmin

--- citas/admin.py
+++ citas/admin.py
@@ -7,3 +7,18 @@ class DentistaAdmin(admin.ModelAdmin):
     list_display = ("nombre", "activo")
     list_filter = ("activo",)
     search_fields = ("nombre",)
+
+@admin.register(Cita)
+class CitaAdmin(admin.ModelAdmin):
+    list_display = (
+        "id_cita",
+        "nombre_paciente",
+        "dni_paciente",
+        "fecha",
+        "hora",
+        "dentista",
+        "estado",
+    )
+    list_filter = ("estado", "fecha", "dentista")
+    search_fields = ("id_cita", "dni_paciente", "nombre_paciente")
+    readonly_fields = ("id_cita", "creada_en", "actualizada_en")
