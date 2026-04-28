975d7b2195e27760f0b31bb5cdc9e14e5313696e
iBadVan <143822962+iBadVan@users.noreply.github.com>
2026-04-28 14:46:13 -0500

    Create urls.py
    creacion del archivo urls, para la navegación

---
+++ citas/urls.py
@@ -0,0 +1,13 @@
+"""URLs de la app citas."""
+from django.urls import path
+
+from . import views
+
+app_name = "citas"
+
+urlpatterns = [
+    path("", views.menu, name="menu"),
+    path("registrar/", views.registrar, name="registrar"),
+    path("consultar/", views.consultar, name="consultar"),
+    path("cancelar/", views.cancelar, name="cancelar"),
+]
