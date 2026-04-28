2bff71b31135e715ad752eb3026261e9ce05e834
Mauricio Paolo Lazo Franco <mauricio.lazo@estudiante.ucsm.edu.pe>
2026-04-28 14:48:19 -0500

    Creando Archivo models y creando modelos para horas, motivos y estados

---
+++ citas/models.py
@@ -0,0 +1,33 @@
+"""
+Modelos del Sistema de Citas Odontologicas - Grupo D
+RF-01 Registrar, RF-02 Consultar, RF-03 Cancelar.
+"""
+from datetime import date, datetime
+from django.db import models
+from django.db.models import Q
+
+
+HORAS_DISPONIBLES = [
+    ("08:00", "08:00"),
+    ("09:00", "09:00"),
+    ("10:00", "10:00"),
+    ("11:00", "11:00"),
+    ("14:00", "14:00"),
+    ("15:00", "15:00"),
+    ("16:00", "16:00"),
+    ("17:00", "17:00"),
+]
+
+MOTIVOS = [
+    ("Limpieza", "Limpieza"),
+    ("Extraccion", "Extraccion"),
+    ("Endodoncia", "Endodoncia"),
+    ("Ortodoncia", "Ortodoncia"),
+    ("Revision general", "Revision general"),
+]
+
+ESTADOS = [
+    ("Activa", "Activa"),
+    ("Cancelada", "Cancelada"),
+]
+
